3f86ceb2ccf0e8a07a4a64b7d477486afbf1c779
Ilir Bekteshi <Ilir.Bekteshi@qlik.com>
2020-01-08 10:43:03 +0100

    Update to Conftest 0.16.0; Verify policy

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
-FROM instrumenta/conftest:v0.10.0
+# Using sha instead of tag for instrumenta/conftest:v0.16.0
+FROM instrumenta/conftest@sha256:a825f1662ce371abb894971814e38c0b9463d6b5872d919e7109badaa284e74f
 
 RUN apk add --no-cache \
     jq \
--- entrypoint.sh
+++ entrypoint.sh
@@ -21,10 +21,13 @@ if [ ! -z "$REGISTRY_USER" -a ! -z "$REGISTRY_PWD" ]; then
     printf "$JSON_FMT" "$INPUT_REGISTRY" "$(echo "$REGISTRY_USER:$REGISTRY_PWD" | base64 -w 0)" > $AUTH_FILE
 fi
 
+echo conftest verify -p $INPUT_PATH
+conftest verify -p $INPUT_PATH || exit 1
+
 echo conftest push $INPUT_REGISTRY/$INPUT_BUNDLE_NAME:$INPUT_TAG -p $INPUT_PATH
-conftest push $INPUT_REGISTRY/$INPUT_BUNDLE_NAME:$INPUT_TAG -p $INPUT_PATH
+conftest push $INPUT_REGISTRY/$INPUT_BUNDLE_NAME:$INPUT_TAG -p $INPUT_PATH || exit 1
 
 if [ "$INPUT_TAG_LATEST" == "true" ]; then
     echo conftest push $INPUT_REGISTRY/$INPUT_BUNDLE_NAME:latest -p $INPUT_PATH
-    conftest push $INPUT_REGISTRY/$INPUT_BUNDLE_NAME:latest -p $INPUT_PATH
+    conftest push $INPUT_REGISTRY/$INPUT_BUNDLE_NAME:latest -p $INPUT_PATH || exit 1
 fi
